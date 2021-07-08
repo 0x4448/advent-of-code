43d7331b2a8371a1b59d6d256263bc91119a013b
0x4448 <86135470+0x4448@users.noreply.github.com>
2021-07-07 21:00:15 -0700

    Add ability to supply input as string argument

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 # Compute the answer when given a module, callable, and input.
+#
+# Test one input:
+# ./run.py 2015.day02 get_area --input "2x3x4"
+#
+# Test all inputs:
+# ./run.py 2015.day02 get_area --input-file 2015/inputs/day02.txt
 
 import argparse
 import importlib
@@ -9,12 +15,16 @@ import pathlib
 parser = argparse.ArgumentParser()
 parser.add_argument("module", type=str)
 parser.add_argument("callable", type=str)
-parser.add_argument("input", type=pathlib.Path)
+parser.add_argument("--input", type=str)
+parser.add_argument("--input-file", type=pathlib.Path)
 args = parser.parse_args()
 
 mod = importlib.import_module(args.module)
 
-with open(args.input) as fp:
-    data = fp.read()
+if args.input:
+    data = args.input
+elif args.input_file:
+    with open(args.input_file) as fp:
+        data = fp.read()
 
 print(getattr(mod, args.callable)(data))
